897063bb69d5b5ec26f7263bf4d2a803a3af82de
yaroona224 <162414446+yaroona224@users.noreply.github.com>
2025-09-16 12:42:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(page_title="CardBlur", page_icon="🪪")
 st.title("🪪 CardBlur")
-st.write("Hello from Streamlit! (Step 1)")
+
+img_file = st.file_uploader("Upload an image (JPG/PNG)", type=["jpg","jpeg","png"])
+if img_file:
+    img = Image.open(img_file).convert("RGB")
+    st.image(img, caption="Uploaded", use_column_width=True)
+
 st.caption("Made by Yara & Shatha 💜")
